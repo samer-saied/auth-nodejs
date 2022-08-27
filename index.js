f3ebe095903c289e3edfcd349adc5c5ac073c32b
Samer Saied <samerpasha@hotmail.com>
2022-08-27 04:06:19 +0200

    updated 270822-2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const strings = require("./core/strings");
 const authRouter = require("./routes/auth");
 const imageRouter = require("./routes/upload-image");
+const path = require('path')
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -15,7 +16,7 @@ app.use(express.static('uploads'));
 app.set('view engine', 'ejs');
 
 // Serve "static" 
-app.use(express.static('uploads'))
+app.use(express.static(path.join(__dirname, '/uploads')))
 
 
 app.use(authRouter);
